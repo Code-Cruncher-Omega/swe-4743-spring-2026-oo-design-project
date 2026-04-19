9684e0cf3cddd4d39d5bdefa728519a710509025
Code-Cruncher-Omega <earella6@gmail.com>
2026-04-19 01:11:25 -0400

    Update ThermostatState.java

--- backend/src/main/java/edu/kennesaw/smarthome/domain/device/thermostat/ThermostatState.java
+++ backend/src/main/java/edu/kennesaw/smarthome/domain/device/thermostat/ThermostatState.java
@@ -4,6 +4,8 @@ import edu.kennesaw.smarthome.domain.device.ActionResult;
 import edu.kennesaw.smarthome.domain.device.DeviceState;
 
 public interface ThermostatState extends DeviceState<Thermostat, ThermostatAction> {
+    // updateAmbientTemperature checks if TODO
+    
     @Override
     ActionResult execute(Thermostat context, ThermostatAction action);
     // How the ambient temperature is updated depends on the current state.
